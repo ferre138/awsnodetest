00d2689a02dc23ea16bf771c0dc6148d261d2306
Jayme Cunha <jaymecunha@gmail.com>
2018-08-04 02:56:15 +0000

    edited app.js and installed ejs and body-parser packages

--- app.js
+++ app.js
@@ -1,9 +1,13 @@
-const express = require('express');
-const app = express();
+var express	= require('express'),
+    app 	= express(),
+    bodyParser	= require("body-parser");
 
+app.use(bodyParser.urlencoded({extended: true}));
+app.set("view engine", "ejs");
 app.use(express.static('public'));
+
 app.get('/', function(req, res){ 
-	res.send('HEY!');
+	res.send('HEY! YOU THRE!');
 });
 
 app.listen(3000, () => console.log('Server running on port 3000'));
